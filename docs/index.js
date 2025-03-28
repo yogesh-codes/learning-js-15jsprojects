a17dc6b8a9c3924c6eaf724feef55d96494f9f7d
Yogesh <139562574+yogesh-codes@users.noreply.github.com>
2025-03-28 13:45:55 +0530

    minor- hamburger animation works onclick

--- docs/index.js
+++ docs/index.js
@@ -2,6 +2,7 @@
 
 let is_mobile_navlinks_hidden=false;
 
+
 //DOM elements
 let hamburger=document.querySelector(".hamburger-svg-container");
 
@@ -9,12 +10,14 @@ let hamburger=document.querySelector(".hamburger-svg-container");
 hamburger.addEventListener("click",function() {
     if (is_mobile_navlinks_hidden){
         document.querySelector(".mobile-navlinks-container").style.height='8em';
-        is_mobile_navlinks_hidden=false
+        hamburger.style.transform="rotate(90deg)";
+        is_mobile_navlinks_hidden=false;
     }
 
     else {
         document.querySelector(".mobile-navlinks-container").style.height='0em';
         is_mobile_navlinks_hidden=true
+        hamburger.style.transform="rotate(0deg)";
     }
     
 })
