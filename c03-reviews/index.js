9b7ae461e7a82b2d1c8273d7c8beccd82f764fc4
Yogesh <139562574+yogesh-codes@users.noreply.github.com>
2025-03-28 05:38:06 +0530

    successfully used faker to generate identities for card testing

--- c03-reviews/index.js
+++ c03-reviews/index.js
@@ -1,15 +1,17 @@
 
-function generate_lorem() {
-    
-    return Text;
-}
+import {faker} from 'https:esm.sh/@faker-js/faker';
 
+
+document.addEventListener('DOMContentLoaded',() => {
 
 // DOM ELEMENTs
-button_random=document.querySelector(".button-random");
-img_face=document.querySelector(".author-profilepic");
-img_face_container=document.querySelector(".author-profilepic-container");
-
+const button_random=document.querySelector(".button-random");
+let img_face=document.querySelector(".author-profilepic");
+let img_face_container=document.querySelector(".author-profilepic-container");
+const review_card=document.querySelector(".review-cards");
+const author_name=document.querySelector(".author-name");
+const author_designation=document.querySelector(".author-designation");
+const author_description=document.querySelector(".author-description");
 
 // button_random.addEventListener("click",function() {
 //     console.log("You clicken");
@@ -19,40 +21,76 @@ img_face_container=document.querySelector(".author-profilepic-container");
 
 // });
 
-// Add event listeners ---------------------
 
-button_random.addEventListener("click", () => {
 
+function generate_random_author(){
+    let sex= faker.person.sex();
+    const review_author = {
+        'sex':sex,
+        'fullName':faker.person.fullName({sex:sex}),
+        'bio':faker.person.bio(),
+        'jobTitle':faker.person.jobTitle(),
+        'profile_pic_url':faker.image.personPortrait({sex:sex}),
+        'description':faker.lorem.paragraph()
+        // profile_pic:"https://thispersondoesnotexist.com"
+    };
+
+    console.log(review_author);
+
+    return review_author;    
+};
+
+function update_image_to_spinner(){
     // Replace container children to spinner:
-    spinner=document.createElement("div");
+    let spinner=document.createElement("div");
     spinner.className="spinner"
 
     img_face_container.replaceChildren(spinner);
     
+
+};
+
+
+function load_image(url){
+        //Setting up variables for load
+        const temp = new Image();
     
-    //Setting up variables for load
-    const temp = new Image();
-    const url = "https://thispersondoesnotexist.com" //+"/?cache=" + Date.now();
+        //Setting up event listener, this is not where this code runs.
+        temp.onload = () => {
+            console.log("step2- onload() is successfully triggered");
+    
+            img_face=temp; // ✅ Set the real image only after it's loaded
+            console.log("bts set img_facesrc to url")
+            img_face_container.replaceChildren(img_face);
+            console.log("container's child is replaced")
+            
+            console.log("image loaded.")
+        };
+    
+        // loading process runs NOW, this code triggers the loading,
+        // after download, temp.onload() is auto triggered
+        temp.src = url;
+        console.log("step 1- temp.src=url executed.")
+};
+
+function update_text_details(author) {
+    console.log(author);
+    author_name.textContent=author['fullName'];
+    author_designation.textContent=author['jobTitle'];
+    author_description.textContent=author['description'];
+}
 
 
-    //Setting up event listener, this is not where this code runs.
-    temp.onload = () => {
-        console.log("step2- onload() is successfully triggered");
+// Add event listeners ---------------------
 
-        img_face=temp; // ✅ Set the real image only after it's loaded
-        console.log("bts set img_facesrc to url")
-        img_face_container.replaceChildren(img_face);
-        console.log("container's child is replaced")
-        
-        console.log("image loaded.")
-    };
-
-    // loading process runs NOW, this code triggers the loading,
-    // after download, temp.onload() is auto triggered
-    temp.src = url;
-    console.log("step 1- temp.src=url executed.")
-
+button_random.addEventListener("click", () => {
+    let author=generate_random_author();
+    update_image_to_spinner();
+    load_image(author['profile_pic_url']);
+    update_text_details(author);
 });
 
 
+
+});
 
